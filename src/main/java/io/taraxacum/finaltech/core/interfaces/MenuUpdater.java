5984206d41469479ac5b93da3831ae2bcb4c3e97
ljc0424 <1979328077@qq.com>
2023-05-15 10:53:34 +0800

    feat: update MenuUpdater

--- src/main/java/io/taraxacum/finaltech/core/interfaces/MenuUpdater.java
+++ src/main/java/io/taraxacum/finaltech/core/interfaces/MenuUpdater.java
@@ -4,7 +4,8 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.util.ConfigUtil;
 import io.taraxacum.libs.plugin.util.ItemStackUtil;
-import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import org.bukkit.Location;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -16,8 +17,15 @@ import javax.annotation.Nonnull;
  */
 public interface MenuUpdater {
 
-    default void updateMenu(@Nonnull BlockMenu blockMenu, int slot, @Nonnull SlimefunItem slimefunItem, @Nonnull String... text) {
-        ItemStack item = blockMenu.getItemInSlot(slot);
+    default void updateInv(@Nonnull Inventory inventory, int slot, @Nonnull SlimefunItem slimefunItem, @Nonnull String... text) {
+        ItemStack item = inventory.getItem(slot);
+        if(!ItemStackUtil.isItemNull(item)) {
+            ItemStackUtil.setLore(item, ConfigUtil.getStatusMenuLore(FinalTech.getLanguageManager(), slimefunItem, text));
+        }
+    }
+
+    default void updateInv(@Nonnull Inventory inventory, @Nonnull Location location, int slot, @Nonnull SlimefunItem slimefunItem, @Nonnull String... text) {
+        ItemStack item = inventory.getItem(slot);
         if(!ItemStackUtil.isItemNull(item)) {
             ItemStackUtil.setLore(item, ConfigUtil.getStatusMenuLore(FinalTech.getLanguageManager(), slimefunItem, text));
         }
