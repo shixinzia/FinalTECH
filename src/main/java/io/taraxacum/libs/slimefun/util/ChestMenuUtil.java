d5f359baa55d3790ccaeb2ed7539c7f238a56160
ljc0424 <1979328077@qq.com>
2023-06-12 11:29:26 +0800

    feat: new method of ChestMenuUtil. Now we can use InventoryTemplate to generate BlockMenuPreset.

--- src/main/java/io/taraxacum/libs/slimefun/util/ChestMenuUtil.java
+++ src/main/java/io/taraxacum/libs/slimefun/util/ChestMenuUtil.java
@@ -1,19 +1,109 @@
 package io.taraxacum.libs.slimefun.util;
 
+import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
+import io.taraxacum.libs.plugin.interfaces.CloseFunctionInventory;
+import io.taraxacum.libs.plugin.interfaces.InventoryTemplate;
+import io.taraxacum.libs.plugin.interfaces.LogicInventory;
+import io.taraxacum.libs.plugin.interfaces.OpenFunctionInventory;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ClickAction;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
+import me.mrCookieSlime.Slimefun.api.inventory.DirtyChestMenu;
+import me.mrCookieSlime.Slimefun.api.item_transport.ItemTransportFlow;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
  * @author Final_ROOT
  */
 public class ChestMenuUtil {
+
+    @Nonnull
+    public static BlockMenuPreset warpBlockMenuPreset(InventoryTemplate inventoryTemplate) {
+        return new BlockMenuPreset(inventoryTemplate.getId(), inventoryTemplate.getName()) {
+            @Override
+            public void init() {
+                int maxSlot = 1;
+                for (Map.Entry<Integer, ItemStack> entry : inventoryTemplate.getDefaultItemStacks().entrySet()) {
+                    this.addItem(entry.getKey(), entry.getValue());
+                    this.addMenuClickHandler(entry.getKey(), ChestMenuUtils.getEmptyClickHandler());
+                    maxSlot = Math.max(maxSlot, entry.getKey());
+                }
+
+                if(maxSlot / 9 + 1 != inventoryTemplate.getSize() / 9) {
+                    this.setSize(inventoryTemplate.getSize());
+                }
+            }
+
+            @Override
+            public void newInstance(@Nonnull BlockMenu blockMenu, @Nonnull Block block) {
+                super.newInstance(blockMenu, block);
+                Location location = block.getLocation();
+                Inventory inventory = blockMenu.toInventory();
+
+                if(inventoryTemplate instanceof LogicInventory logicInventory) {
+                    logicInventory.updateInventory(inventory, location);
+                }
+
+                if(inventoryTemplate instanceof OpenFunctionInventory openFunctionInventory) {
+                    blockMenu.addMenuOpeningHandler(player -> openFunctionInventory.onOpen(player, location, inventory));
+                }
+
+                if(inventoryTemplate instanceof CloseFunctionInventory closeFunctionInventory) {
+                    blockMenu.addMenuCloseHandler(player -> closeFunctionInventory.onClose(player, location, inventory));
+                }
+
+                for (int slot = 0; slot < inventoryTemplate.getSize(); slot++) {
+                    Consumer<InventoryClickEvent> eventConsumer = inventoryTemplate.onClick(location, slot);
+                    if (eventConsumer != null) {
+                        blockMenu.addMenuClickHandler(slot, warpByConsumer(eventConsumer));
+                    }
+                }
+            }
+
+            @Override
+            public boolean canOpen(@Nonnull Block block, @Nonnull Player player) {
+                return inventoryTemplate.canOpen(player, block.getLocation());
+            }
+
+            @Override
+            public int[] getSlotsAccessedByItemTransport(ItemTransportFlow itemTransportFlow) {
+                if (inventoryTemplate instanceof LogicInventory logicInventory) {
+                    LogicInventory.RequestType requestType = getItemTransportFlow(itemTransportFlow);
+                    return requestType == null ? logicInventory.requestSlots() : logicInventory.requestSlots(requestType);
+                }
+                return new int[0];
+            }
+
+            @Override
+            public int[] getSlotsAccessedByItemTransport(DirtyChestMenu chestMenu, ItemTransportFlow itemTransportFlow, ItemStack itemStack) {
+                if (inventoryTemplate instanceof LogicInventory logicInventory) {
+                    LogicInventory.RequestType requestType = getItemTransportFlow(itemTransportFlow);
+                    if (requestType == null) {
+                        return logicInventory.requestSlots();
+                    } else {
+                        if (chestMenu instanceof BlockMenu blockMenu) {
+                            return logicInventory.requestSlots(requestType, itemStack, chestMenu.toInventory(), blockMenu.getLocation());
+                        } else {
+                            return logicInventory.requestSlots(requestType);
+                        }
+                    }
+                }
+                return this.getSlotsAccessedByItemTransport(itemTransportFlow);
+            }
+        };
+    }
 
     @Nonnull
     public static ChestMenu.AdvancedMenuClickHandler warpByConsumer(@Nonnull Consumer<InventoryClickEvent> consumer) {
@@ -49,5 +139,13 @@ public class ChestMenuUtil {
                 return ClickType.LEFT;
             }
         }
+    }
+
+    @Nullable
+    public static LogicInventory.RequestType getItemTransportFlow(@Nullable ItemTransportFlow itemTransportFlow) {
+        return itemTransportFlow == null ? null : switch (itemTransportFlow) {
+            case INSERT -> LogicInventory.RequestType.INPUT;
+            case WITHDRAW -> LogicInventory.RequestType.OUTPUT;
+        };
     }
 }
