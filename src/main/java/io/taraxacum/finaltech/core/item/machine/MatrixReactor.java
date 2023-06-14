8cc20769e52a3a915ba17a3fc2ea1ff002cc874a
ljc0424 <1979328077@qq.com>
2023-06-14 09:59:01 +0800

    refactor: update MatrixReactor

--- src/main/java/io/taraxacum/finaltech/core/item/machine/MatrixReactor.java
+++ src/main/java/io/taraxacum/finaltech/core/item/machine/MatrixReactor.java
@@ -6,13 +6,12 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.taraxacum.common.util.JavaUtil;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.core.interfaces.MenuUpdater;
 import io.taraxacum.finaltech.core.interfaces.RecipeItem;
-import io.taraxacum.finaltech.core.menu.AbstractMachineMenu;
-import io.taraxacum.finaltech.core.menu.machine.MatrixReactorMenu;
+import io.taraxacum.finaltech.core.inventory.AbstractMachineInventory;
+import io.taraxacum.finaltech.core.inventory.simple.MatrixReactorInventory;
 import io.taraxacum.finaltech.setup.FinalTechItems;
 import io.taraxacum.libs.plugin.dto.ItemAmountWrapper;
 import io.taraxacum.libs.plugin.dto.LocationData;
@@ -34,6 +33,7 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -45,9 +45,26 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
     private final String keyCount = "c";
     private final int difficulty = ConfigUtil.getOrDefaultItemSetting(80, this, "difficulty");
     private final Set<String> notAllowedIdList = new HashSet<>(ConfigUtil.getItemStringList(this, "not-allowed-id"));
+    private int[] itemPhonyInputSlot;
+    private int[] itemInputSlot;
+    private int statusSlot;
+    private int[] orderedDustSlot;
+    private int[] unorderedDustSlot;
 
     public MatrixReactor(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
+    }
+
+    @Nullable
+    @Override
+    protected AbstractMachineInventory setMachineInventory() {
+        MatrixReactorInventory matrixReactorInventory = new MatrixReactorInventory(this);
+        this.itemPhonyInputSlot = matrixReactorInventory.itemPhonyInputSlot;
+        this.itemInputSlot = matrixReactorInventory.itemInputSlot;
+        this.statusSlot = matrixReactorInventory.statusSlot;
+        this.orderedDustSlot = matrixReactorInventory.orderedDustInputSlot;
+        this.unorderedDustSlot = matrixReactorInventory.unorderedDustInputSlot;
+        return matrixReactorInventory;
     }
 
     @Nonnull
@@ -59,29 +76,23 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
     @Nonnull
     @Override
     protected BlockBreakHandler onBlockBreak() {
-        return MachineUtil.simpleBlockBreakerHandler(FinalTech.getLocationDataService(), this.getId(), MatrixReactorMenu.ITEM_PHONY_INPUT_SLOT);
-    }
-
-    @Nonnull
-    @Override
-    protected AbstractMachineMenu setMachineMenu() {
-        return new MatrixReactorMenu(this);
+        return MachineUtil.simpleBlockBreakerHandler(FinalTech.getLocationDataService(), this, this.itemPhonyInputSlot);
     }
 
     @Override
     protected void tick(@Nonnull Block block, @Nonnull SlimefunItem slimefunItem, @Nonnull LocationData locationData) {
-        Location location = block.getLocation();
         Inventory inventory = FinalTech.getLocationDataService().getInventory(locationData);
         if(inventory == null) {
             return;
         }
-        ItemStack itemStack = inventory.getItem(MatrixReactorMenu.ITEM_INPUT_SLOT[0]);
+
+        ItemStack itemStack = inventory.getItem(this.itemInputSlot[0]);
 
         if (ItemStackUtil.isItemNull(itemStack)) {
             FinalTech.getLocationDataService().setLocationData(locationData, this.keyItem, null);
             FinalTech.getLocationDataService().setLocationData(locationData, this.keyCount, "0");
             if (!inventory.getViewers().isEmpty()) {
-                this.updateInv(inventory, MatrixReactorMenu.STATUS_SLOT, this,
+                this.updateInv(inventory, this.statusSlot, this,
                         "0",
                         String.valueOf(this.difficulty));
             }
@@ -90,12 +101,14 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
             FinalTech.getLocationDataService().setLocationData(locationData, this.keyItem, null);
             FinalTech.getLocationDataService().setLocationData(locationData, this.keyCount, "0");
             if (!inventory.getViewers().isEmpty()) {
-                this.updateInv(inventory, MatrixReactorMenu.STATUS_SLOT, this,
+                this.updateInv(inventory, this.statusSlot, this,
                         "0",
                         String.valueOf(this.difficulty));
             }
             JavaPlugin javaPlugin = this.getAddon().getJavaPlugin();
-            javaPlugin.getServer().getScheduler().runTask(javaPlugin, () -> InventoryUtil.dropItems(inventory, location, MatrixReactorMenu.ITEM_INPUT_SLOT));
+            // TODO safe to do this?
+            Location location = locationData.getLocation();
+            javaPlugin.getServer().getScheduler().runTask(javaPlugin, () -> InventoryUtil.dropItems(inventory, location, this.itemInputSlot));
             return;
         }
 
@@ -105,17 +118,15 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
             stringItem = ItemStackUtil.stringToItemStack(keyItemStr);
         }
 
-        boolean match = true;
-
-        int[] orderedDustItemSlots = new int[MatrixReactorMenu.ORDERED_DUST_INPUT_SLOT.length];
-        int[] unorderedDustItemSlots = new int[MatrixReactorMenu.UNORDERED_DUST_INPUT_SLOT.length];
-
         int amount = itemStack.getAmount();
+        int[] orderedDustItemSlots = new int[this.orderedDustSlot.length];
+        int[] unorderedDustItemSlots = new int[this.unorderedDustSlot.length];
         int orderedDustItemSlotsP = 0;
         int orderedDustItemCount = 0;
         int unorderedDustItemSlotsP = 0;
         int unorderedDustItemCount = 0;
-        for (int slot : MatrixReactorMenu.ORDERED_DUST_INPUT_SLOT) {
+
+        for (int slot : this.orderedDustSlot) {
             if (FinalTechItems.ORDERED_DUST.verifyItem(inventory.getItem(slot))) {
                 orderedDustItemSlots[orderedDustItemSlotsP++] = slot;
                 orderedDustItemCount += inventory.getItem(slot).getAmount();
@@ -124,8 +135,9 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
                 }
             }
         }
+
         if (orderedDustItemCount >= amount) {
-            for (int slot : MatrixReactorMenu.UNORDERED_DUST_INPUT_SLOT) {
+            for (int slot : this.unorderedDustSlot) {
                 if (FinalTechItems.UNORDERED_DUST.verifyItem(inventory.getItem(slot))) {
                     unorderedDustItemSlots[unorderedDustItemSlotsP++] = slot;
                     unorderedDustItemCount += inventory.getItem(slot).getAmount();
@@ -135,9 +147,8 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
                 }
             }
         }
-        if (orderedDustItemCount < amount || unorderedDustItemCount < amount) {
-            match = false;
-        }
+
+        boolean match = orderedDustItemCount >= amount && unorderedDustItemCount >= amount;
 
         if (!match) {
             String keyCountStr = FinalTech.getLocationDataService().getLocationData(locationData, this.keyCount);
@@ -171,8 +182,8 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
                 FinalTech.getLocationDataService().setLocationData(locationData, this.keyItem, ItemStackUtil.itemStackToString(itemStack));
 
                 int count;
-                if (FinalTechItems.ITEM_PHONY.verifyItem(inventory.getItem(MatrixReactorMenu.ITEM_PHONY_INPUT_SLOT[0]))) {
-                    ItemStack itemPhony = inventory.getItem(MatrixReactorMenu.ITEM_PHONY_INPUT_SLOT[0]);
+                if (FinalTechItems.ITEM_PHONY.verifyItem(inventory.getItem(this.itemPhonyInputSlot[0]))) {
+                    ItemStack itemPhony = inventory.getItem(this.itemPhonyInputSlot[0]);
                     itemPhony.setAmount(itemPhony.getAmount() - 1);
                     count = 1;
                 } else {
@@ -183,8 +194,8 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
             } else {
                 String keyCountStr = FinalTech.getLocationDataService().getLocationData(locationData, this.keyCount);
                 int count = keyCountStr != null ? Integer.parseInt(keyCountStr) : 0;
-                if (FinalTechItems.ITEM_PHONY.verifyItem(inventory.getItem(MatrixReactorMenu.ITEM_PHONY_INPUT_SLOT[0])) && inventory.getItem(MatrixReactorMenu.ITEM_PHONY_INPUT_SLOT[0]).getAmount() >= amount + count && amount + count <= ConstantTableUtil.ITEM_MAX_STACK) {
-                    ItemStack itemPhony = inventory.getItem(MatrixReactorMenu.ITEM_PHONY_INPUT_SLOT[0]);
+                if (FinalTechItems.ITEM_PHONY.verifyItem(inventory.getItem(this.itemPhonyInputSlot[0])) && inventory.getItem(this.itemPhonyInputSlot[0]).getAmount() >= amount + count) {
+                    ItemStack itemPhony = inventory.getItem(this.itemPhonyInputSlot[0]);
                     itemPhony.setAmount(itemPhony.getAmount() - count - amount);
                     count++;
                 } else {
@@ -196,7 +207,7 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
                         FinalTech.getLocationDataService().setLocationData(locationData, this.keyItem, null);
                         FinalTech.getLocationDataService().setLocationData(locationData, this.keyCount, "0");
                         if (!inventory.getViewers().isEmpty()) {
-                            this.updateInv(inventory, MatrixReactorMenu.STATUS_SLOT, this,
+                            this.updateInv(inventory, this.statusSlot, this,
                                     "0",
                                     String.valueOf(difficulty));
                         }
@@ -212,7 +223,7 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
         }
 
         if (!inventory.getViewers().isEmpty()) {
-            this.updateInv(inventory, MatrixReactorMenu.STATUS_SLOT, this,
+            this.updateInv(inventory, this.statusSlot, this,
                     JavaUtil.getFirstNotNull(FinalTech.getLocationDataService().getLocationData(locationData, this.keyCount), "0"),
                     String.valueOf(difficulty));
         }
@@ -227,13 +238,14 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
     public void registerDefaultRecipes() {
         RecipeUtil.registerDescriptiveRecipe(FinalTech.getLanguageManager(), this,
                 String.valueOf(this.difficulty),
-                String.format("%.2f", Slimefun.getTickerTask().getTickRate() / 20.0));
+                ConstantTableUtil.SLIMEFUN_TICK_INTERVAL);
     }
 
     private boolean allowedItem(@Nonnull ItemStack item) {
         if (Tag.SHULKER_BOXES.isTagged(item.getType()) || Material.BUNDLE.equals(item.getType())) {
             return false;
         }
+
         if (item.hasItemMeta()) {
             ItemMeta itemMeta = item.getItemMeta();
             PersistentDataContainer persistentDataContainer = itemMeta.getPersistentDataContainer();
@@ -245,6 +257,7 @@ public class MatrixReactor extends AbstractMachine implements RecipeItem, MenuUp
                 }
             }
         }
+
         SlimefunItem slimefunItem = SlimefunItem.getByItem(item);
         return slimefunItem == null || !this.notAllowedIdList.contains(slimefunItem.getId());
     }
