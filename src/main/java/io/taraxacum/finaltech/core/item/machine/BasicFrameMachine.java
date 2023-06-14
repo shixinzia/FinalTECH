13f52782c98e75b6dc4e069a6a2ba2b77be4da6f
ljc0424 <1979328077@qq.com>
2023-06-14 10:02:04 +0800

    refactor: update BasicFrameMachine

--- src/main/java/io/taraxacum/finaltech/core/item/machine/BasicFrameMachine.java
+++ src/main/java/io/taraxacum/finaltech/core/item/machine/BasicFrameMachine.java
@@ -8,14 +8,14 @@ import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.core.interfaces.RecipeItem;
+import io.taraxacum.finaltech.core.inventory.AbstractMachineInventory;
+import io.taraxacum.finaltech.core.inventory.limit.BasicFrameMachineInventory;
 import io.taraxacum.libs.plugin.dto.InvWithSlots;
 import io.taraxacum.finaltech.core.dto.SimpleCargoDTO;
 import io.taraxacum.finaltech.core.option.CargoFilter;
 import io.taraxacum.finaltech.core.option.CargoLimit;
 import io.taraxacum.finaltech.core.option.SlotSearchOrder;
 import io.taraxacum.finaltech.core.option.SlotSearchSize;
-import io.taraxacum.finaltech.core.menu.limit.AbstractLimitMachineMenu;
-import io.taraxacum.finaltech.core.menu.limit.BasicFrameMachineMenu;
 import io.taraxacum.finaltech.util.CargoUtil;
 import io.taraxacum.finaltech.util.MachineUtil;
 import io.taraxacum.finaltech.util.RecipeUtil;
@@ -26,6 +26,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * @author Final_ROOT
@@ -33,6 +34,12 @@ import javax.annotation.Nonnull;
 public class BasicFrameMachine extends AbstractMachine implements RecipeItem {
     public BasicFrameMachine(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
+    }
+
+    @Nullable
+    @Override
+    protected AbstractMachineInventory setMachineInventory() {
+        return new BasicFrameMachineInventory(this);
     }
 
     @Nonnull
@@ -47,22 +54,28 @@ public class BasicFrameMachine extends AbstractMachine implements RecipeItem {
         return MachineUtil.simpleBlockBreakerHandler(FinalTech.getLocationDataService(), this);
     }
 
-    @Nonnull
-    @Override
-    protected AbstractLimitMachineMenu setMachineMenu() {
-        return new BasicFrameMachineMenu(this);
-    }
-
     @Override
     protected void tick(@Nonnull Block block, @Nonnull SlimefunItem slimefunItem, @Nonnull LocationData locationData) {
         Inventory inventory = FinalTech.getLocationDataService().getInventory(locationData);
-
         if (inventory == null) {
             return;
         }
 
         InventoryUtil.stockSlots(inventory, this.getInputSlot());
-        CargoUtil.doSimpleCargoStrongSymmetry(new SimpleCargoDTO(FinalTech.getLocationDataService(), new InvWithSlots(inventory, this.getInputSlot()), block, SlotSearchSize.VALUE_INPUTS_ONLY, SlotSearchOrder.VALUE_ASCENT, new InvWithSlots(inventory, this.getOutputSlot()), block, SlotSearchSize.VALUE_OUTPUTS_ONLY, SlotSearchOrder.VALUE_ASCENT, this.getInputSlot().length * 64, CargoLimit.VALUE_ALL, CargoFilter.VALUE_BLACK, inventory, new int[0]));
+        CargoUtil.doSimpleCargoStrongSymmetry(new SimpleCargoDTO(FinalTech.getLocationDataService(),
+                new InvWithSlots(inventory, this.getInputSlot()),
+                block,
+                SlotSearchSize.VALUE_INPUTS_ONLY,
+                SlotSearchOrder.VALUE_ASCENT,
+                new InvWithSlots(inventory, this.getOutputSlot()),
+                block,
+                SlotSearchSize.VALUE_OUTPUTS_ONLY,
+                SlotSearchOrder.VALUE_ASCENT,
+                this.getInputSlot().length * 64,
+                CargoLimit.VALUE_ALL,
+                CargoFilter.VALUE_BLACK,
+                inventory,
+                new int[0]));
         InventoryUtil.stockSlots(inventory, this.getOutputSlot());
     }
 
