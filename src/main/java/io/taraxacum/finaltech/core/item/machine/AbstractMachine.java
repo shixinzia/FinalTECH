f7bf934ecb58fe6be84a1cc53556f3d97211dbe4
ljc0424 <1979328077@qq.com>
2023-06-14 09:32:46 +0800

    feat: optimization for AbstractMachine. Now it use InventoryTemplate to generate inventory

--- src/main/java/io/taraxacum/finaltech/core/item/machine/AbstractMachine.java
+++ src/main/java/io/taraxacum/finaltech/core/item/machine/AbstractMachine.java
@@ -9,10 +9,12 @@ import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.core.item.AbstractMySlimefunItem;
-import io.taraxacum.finaltech.core.menu.AbstractMachineMenu;
+import io.taraxacum.finaltech.core.inventory.AbstractMachineInventory;
+import io.taraxacum.libs.plugin.interfaces.LogicInventory;
 import io.taraxacum.libs.slimefun.dto.LocationBlockStorageData;
 import io.taraxacum.libs.plugin.dto.LocationData;
 import io.taraxacum.libs.slimefun.dto.LocationDatabaseData;
+import io.taraxacum.libs.slimefun.util.ChestMenuUtil;
 import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
 import me.mrCookieSlime.Slimefun.Objects.handlers.BlockTicker;
 import org.bukkit.block.Block;
@@ -25,25 +27,29 @@ import javax.annotation.Nullable;
  * @author Final_ROOT
  */
 public abstract class AbstractMachine extends AbstractMySlimefunItem {
-    private AbstractMachineMenu menu;
+    private AbstractMachineInventory inventoryTemplate;
 
     public AbstractMachine(@Nonnull ItemGroup itemGroup, @Nonnull SlimefunItemStack item, @Nonnull RecipeType recipeType, @Nonnull ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
+        if (FinalTech.getMultiThreadLevel() >= 2) {
+            FinalTech.addAsyncSlimefunItem(this.getId());
+        } else if(!this.isSynchronized() && FinalTech.getMultiThreadLevel() >= 1) {
+            FinalTech.addAsyncSlimefunItem(this.getId());
+        }
     }
 
     @Override
     public void preRegister() {
         super.preRegister();
 
+        this.inventoryTemplate = this.setMachineInventory();
+        if(this.inventoryTemplate != null) {
+            this.inventoryTemplate.init();
+            ChestMenuUtil.warpBlockMenuPreset(this.inventoryTemplate);
+        }
+
         this.addItemHandler(this.onBlockBreak());
         this.addItemHandler(this.onBlockPlace());
-        this.menu = this.setMachineMenu();
-
-        if (FinalTech.getMultiThreadLevel() == 2) {
-            this.getAddon().getJavaPlugin().getLogger().info(this.getId() + "(" + this.getItemName() + ")" + " is optimized for multi-thread！！！");
-        } else if (!this.isSynchronized() && (FinalTech.getMultiThreadLevel() == 1 || FinalTech.isAsyncSlimefunItem(this.getId()))) {
-            this.getAddon().getJavaPlugin().getLogger().info(this.getId() + "(" + this.getItemName() + ")" + " is optimized for multi-thread！！！");
-        }
 
         BlockTicker blockTicker;
         if (FinalTech.getMultiThreadLevel() == 2) {
@@ -95,26 +101,22 @@ public abstract class AbstractMachine extends AbstractMySlimefunItem {
                     AbstractMachine.this.uniqueTick();
                 }
             };
-            if(!this.isSynchronized() && FinalTech.getMultiThreadLevel() >= 1) {
-                FinalTech.addAsyncSlimefunItem(this.getId());
-            }
         }
         this.addItemHandler(blockTicker);
     }
 
     @Nonnull
     public final int[] getInputSlot() {
-        return this.menu == null ? new int[0] : this.menu.getInputSlot();
+        return this.inventoryTemplate instanceof LogicInventory logicInventory ? logicInventory.requestSlots(LogicInventory.RequestType.INPUT) : new int[0];
     }
 
     @Nonnull
     public final int[] getOutputSlot() {
-        return this.menu == null ? new int[0] : this.menu.getOutputSlot();
+        return this.inventoryTemplate instanceof LogicInventory logicInventory ? logicInventory.requestSlots(LogicInventory.RequestType.OUTPUT) : new int[0];
     }
 
-    protected void uniqueTick() {
-
-    }
+    @Nullable
+    protected abstract AbstractMachineInventory setMachineInventory();
 
     @Nonnull
     protected abstract BlockPlaceHandler onBlockPlace();
@@ -122,12 +124,13 @@ public abstract class AbstractMachine extends AbstractMySlimefunItem {
     @Nonnull
     protected abstract BlockBreakHandler onBlockBreak();
 
-    @Nullable
-    protected abstract AbstractMachineMenu setMachineMenu();
-
     protected void tick(@Nonnull Block block, @Nonnull SlimefunItem slimefunItem, @Nonnull LocationData locationData) {
 
     }
 
     protected abstract boolean isSynchronized();
+
+    protected void uniqueTick() {
+
+    }
 }
