458b4d6225ec0f4628d7625e2cdabde16897e04f
ljc0424 <1979328077@qq.com>
2023-06-14 10:00:51 +0800

    refactor: update EntropySeed

--- src/main/java/io/taraxacum/finaltech/core/item/machine/EntropySeed.java
+++ src/main/java/io/taraxacum/finaltech/core/item/machine/EntropySeed.java
@@ -9,9 +9,9 @@ import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.core.interfaces.RecipeItem;
-import io.taraxacum.finaltech.core.item.machine.range.point.EquivalentConcept;
-import io.taraxacum.finaltech.core.menu.AbstractMachineMenu;
+import io.taraxacum.finaltech.core.inventory.AbstractMachineInventory;
 import io.taraxacum.finaltech.setup.FinalTechItemStacks;
+import io.taraxacum.finaltech.setup.FinalTechItems;
 import io.taraxacum.finaltech.util.ConfigUtil;
 import io.taraxacum.finaltech.util.RecipeUtil;
 import io.taraxacum.libs.plugin.dto.LocationData;
@@ -38,6 +38,12 @@ public class EntropySeed extends AbstractMachine implements RecipeItem {
 
     public EntropySeed(@Nonnull ItemGroup itemGroup, @Nonnull SlimefunItemStack item, @Nonnull RecipeType recipeType, @Nonnull ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
+    }
+
+    @Nullable
+    @Override
+    protected AbstractMachineInventory setMachineInventory() {
+        return null;
     }
 
     @Nonnull
@@ -67,12 +73,6 @@ public class EntropySeed extends AbstractMachine implements RecipeItem {
         };
     }
 
-    @Nullable
-    @Override
-    protected AbstractMachineMenu setMachineMenu() {
-        return null;
-    }
-
     @Override
     protected void tick(@Nonnull Block block, @Nonnull SlimefunItem slimefunItem, @Nonnull LocationData locationData) {
         // TODO optimization
@@ -83,19 +83,21 @@ public class EntropySeed extends AbstractMachine implements RecipeItem {
             FinalTech.getLocationDataService().clearLocationData(location);
             JavaPlugin javaPlugin = this.getAddon().getJavaPlugin();
             javaPlugin.getServer().getScheduler().runTaskLaterAsynchronously(javaPlugin, () -> {
-                if (location.getBlock().getType().equals(EntropySeed.this.getItem().getType())
+                if (location.getChunk().isLoaded()
+                        && location.getBlock().getType().equals(this.getItem().getType())
                         && FinalTech.getLocationDataService().getLocationData(location) == null
                         && FinalTech.getLocationDataService() instanceof SlimefunLocationDataService slimefunLocationDataService) {
                     LocationData tempLocationdata = slimefunLocationDataService.getOrCreateEmptyLocationData(location, FinalTechItemStacks.EQUIVALENT_CONCEPT.getItemId());
-                    FinalTech.getLocationDataService().setLocationData(tempLocationdata, EquivalentConcept.KEY_LIFE, String.valueOf(EntropySeed.this.equivalentConceptLife));
-                    FinalTech.getLocationDataService().setLocationData(tempLocationdata, EquivalentConcept.KEY_RANGE, String.valueOf(EntropySeed.this.equivalentConceptRange));
+                    FinalTech.getLocationDataService().setLocationData(tempLocationdata, FinalTechItems.EQUIVALENT_CONCEPT.keyLife, String.valueOf(this.equivalentConceptLife));
+                    FinalTech.getLocationDataService().setLocationData(tempLocationdata, FinalTechItems.EQUIVALENT_CONCEPT.keyRange, String.valueOf(this.equivalentConceptRange));
                 }
             }, Slimefun.getTickerTask().getTickRate() + 1);
         } else {
             FinalTech.getLocationDataService().clearLocationData(location);
             JavaPlugin javaPlugin = this.getAddon().getJavaPlugin();
             javaPlugin.getServer().getScheduler().runTaskLaterAsynchronously(javaPlugin, () -> {
-                if (location.getBlock().getType().equals(EntropySeed.this.getItem().getType())
+                if (location.getChunk().isLoaded()
+                        && location.getBlock().getType().equals(this.getItem().getType())
                         && FinalTech.getLocationDataService().getLocationData(location) == null
                         && FinalTech.getLocationDataService() instanceof SlimefunLocationDataService slimefunLocationDataService) {
                     slimefunLocationDataService.getOrCreateEmptyLocationData(location, FinalTechItemStacks.JUSTIFIABILITY.getItemId());
