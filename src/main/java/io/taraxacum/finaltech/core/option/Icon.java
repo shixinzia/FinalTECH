cab3eab02900d2c7ebdd1438cd4704372ea5416d
ljc0424 <1979328077@qq.com>
2023-07-03 10:43:02 +0800

    feat: add new icon

--- src/main/java/io/taraxacum/finaltech/core/option/Icon.java
+++ src/main/java/io/taraxacum/finaltech/core/option/Icon.java
@@ -1,13 +1,16 @@
 package io.taraxacum.finaltech.core.option;
 
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
 import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
 import io.taraxacum.finaltech.FinalTech;
 import io.taraxacum.finaltech.core.item.unusable.module.AbstractQuantityModule;
 import io.taraxacum.libs.plugin.util.ItemStackUtil;
+import io.taraxacum.libs.slimefun.dto.ItemValueTableV2;
+import io.taraxacum.libs.slimefun.interfaces.ShowInfoItem;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -19,31 +22,55 @@ import java.util.List;
  * @author Final_ROOT
  */
 public class Icon {
-    public static final ItemStack QUANTITY_MODULE_ICON = new CustomItemStack(Material.REDSTONE, FinalTech.getLanguageString("option", "ICON", "quantity-module", "name"), FinalTech.getLanguageStringArray("option", "ICON", "quantity-module", "lore"));
+    public static final ItemStack QUANTITY_MODULE_ICON = ItemStackUtil.newItemStack(Material.REDSTONE,
+            FinalTech.getLanguageString("option", "ICON", "quantity-module", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "quantity-module", "lore"));
 
     public static final ItemStack BORDER_ICON = ChestMenuUtils.getBackground();
-//    public static final ItemStack INPUT_BORDER_ICON = new CustomItemStack(Material.BLUE_STAINED_GLASS_PANE, FinalTech.getLanguageString("option", "CARGO_ORDER", "input-border", "name"), FinalTech.getLanguageStringArray("option", "CARGO_ORDER", "input-border", "lore"));
+
     public static final ItemStack INPUT_BORDER_ICON = ChestMenuUtils.getInputSlotTexture();
-//    public static final ItemStack OUTPUT_BORDER_ICON = new CustomItemStack(Material.ORANGE_STAINED_GLASS_PANE, FinalTech.getLanguageString("option", "CARGO_ORDER", "output-border", "name"), FinalTech.getLanguageStringArray("option", "CARGO_ORDER", "output-border", "lore"));
+
     public static final ItemStack OUTPUT_BORDER_ICON = ChestMenuUtils.getOutputSlotTexture();
-    public static final ItemStack SPECIAL_BORDER_ICON = new CustomItemStack(Material.PURPLE_STAINED_GLASS_PANE, " ");
+    public static final ItemStack SPECIAL_BORDER_ICON = ItemStackUtil.newItemStack(Material.PURPLE_STAINED_GLASS_PANE, " ");
 
-    public static final ItemStack NEXT_PAGE_ICON = new CustomItemStack(Material.GREEN_STAINED_GLASS_PANE, FinalTech.getLanguageString("option", "ICON", "next-page", "name"), FinalTech.getLanguageStringArray("option", "ICON", "next-page", "lore"));
-    public static final ItemStack PREVIOUS_PAGE_ICON = new CustomItemStack(Material.RED_STAINED_GLASS_PANE, FinalTech.getLanguageString("option", "ICON", "previous-page", "name"), FinalTech.getLanguageStringArray("option", "ICON", "previous-page", "lore"));
+    public static final ItemStack NEXT_PAGE_ICON = ItemStackUtil.newItemStack(Material.GREEN_STAINED_GLASS_PANE,
+            FinalTech.getLanguageString("option", "ICON", "next-page", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "next-page", "lore"));
+    public static final ItemStack PREVIOUS_PAGE_ICON = ItemStackUtil.newItemStack(Material.RED_STAINED_GLASS_PANE,
+            FinalTech.getLanguageString("option", "ICON", "previous-page", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "previous-page", "lore"));
 
-    public static final ItemStack STATUS_ICON = new CustomItemStack(Material.GREEN_STAINED_GLASS_PANE, FinalTech.getLanguageString("option", "ICON", "status", "name"), FinalTech.getLanguageStringArray("option", "ICON", "status", "lore"));
+    public static final ItemStack STATUS_ICON = ItemStackUtil.newItemStack(Material.GREEN_STAINED_GLASS_PANE,
+            FinalTech.getLanguageString("option", "ICON", "status", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "status", "lore"));
 
-    public static final ItemStack ERROR_ICON = new CustomItemStack(Material.BARRIER, FinalTech.getLanguageString("option", "ICON", "error", "name"), FinalTech.getLanguageStringArray("option", "ICON", "error", "lore"));
+    public static final ItemStack ERROR_ICON = ItemStackUtil.newItemStack(Material.BARRIER,
+            FinalTech.getLanguageString("option", "ICON", "error", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "error", "lore"));
 
-    public static final ItemStack WIKI_ICON = new CustomItemStack(Material.KNOWLEDGE_BOOK, FinalTech.getLanguageString("option", "ICON", "wiki-icon", "name"), FinalTech.getLanguageStringArray("option", "ICON", "wiki-icon", "lore"));
+    public static final ItemStack WIKI_ICON = ItemStackUtil.newItemStack(Material.KNOWLEDGE_BOOK,
+            FinalTech.getLanguageString("option", "ICON", "wiki-icon", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "wiki-icon", "lore"));
 
-    public static final ItemStack RECIPE_ICON = new CustomItemStack(Material.PAPER, FinalTech.getLanguageString("option", "ICON", "recipe-icon", "name"), FinalTech.getLanguageStringArray("option", "ICON", "recipe-icon", "lore"));
+    public static final ItemStack RECIPE_ICON = ItemStackUtil.newItemStack(Material.PAPER,
+            FinalTech.getLanguageString("option", "ICON", "recipe-icon", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "recipe-icon", "lore"));
 
-    public static final ItemStack CONSUMER_ICON = new CustomItemStack(Material.FURNACE, FinalTech.getLanguageString("option", "ICON", "consumer", "name"), FinalTech.getLanguageStringArray("option", "ICON", "consumer", "lore"));
+    public static final ItemStack CONSUMER_ICON = ItemStackUtil.newItemStack(Material.FURNACE,
+            FinalTech.getLanguageString("option", "ICON", "consumer", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "consumer", "lore"));
 
-    public static final ItemStack GENERATOR_ICON = new CustomItemStack(Material.DAYLIGHT_DETECTOR, FinalTech.getLanguageString("option", "ICON", "generator", "name"), FinalTech.getLanguageStringArray("option", "ICON", "generator", "lore"));
+    public static final ItemStack GENERATOR_ICON = ItemStackUtil.newItemStack(Material.DAYLIGHT_DETECTOR,
+            FinalTech.getLanguageString("option", "ICON", "generator", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "generator", "lore"));
 
-    public static final ItemStack CAPACITOR_ICON = new CustomItemStack(Material.YELLOW_STAINED_GLASS, FinalTech.getLanguageString("option", "ICON", "capacitor", "name"), FinalTech.getLanguageStringArray("option", "ICON", "capacitor", "lore"));
+    public static final ItemStack CAPACITOR_ICON = ItemStackUtil.newItemStack(Material.YELLOW_STAINED_GLASS,
+            FinalTech.getLanguageString("option", "ICON", "capacitor", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "capacitor", "lore"));
+
+    public static final ItemStack MAIN_ICON = ItemStackUtil.newItemStack(Material.OBSERVER,
+            FinalTech.getLanguageString("option", "ICON", "main-icon", "name"),
+            FinalTech.getLanguageStringArray("option", "ICON", "main-icon", "lore"));
 
     @Deprecated
     public static int updateQuantityModule(@Nonnull BlockMenu blockMenu, int quantityModuleSlot, int statusSlot) {
@@ -108,5 +135,33 @@ public class Icon {
         }
 
         return amount;
+    }
+
+    @Nonnull
+    public static ItemStack generateInfoIcon(@Nonnull SlimefunItem slimefunItem, @Nonnull Player player) {
+        ItemStack infoIcon = ItemStackUtil.cloneItem(Icon.WIKI_ICON);
+        ItemStackUtil.setLore(infoIcon,
+                FinalTech.getLanguageManager().replaceStringArray(FinalTech.getLanguageStringArray("option", "ICON", "wiki-icon", "lore"),
+                        slimefunItem.getId(),
+                        slimefunItem.getResearch() != null ? slimefunItem.getResearch().getName(player) : FinalTech.getLanguageString("option", "ICON", "wiki-icon", "no-research"),
+                        slimefunItem.getAddon().getName(),
+                        ItemValueTableV2.getInstance().getOrCalItemInputValue(slimefunItem).toString()));
+
+        if (slimefunItem instanceof EnergyNetComponent) {
+            String energyLore = FinalTech.getLanguageString("option", "ICON", "wiki-icon", ((EnergyNetComponent) slimefunItem).getEnergyComponentType().name());
+            ItemStackUtil.addLoresToLast(infoIcon, FinalTech.getLanguageManager().replaceStringArray(FinalTech.getLanguageStringArray("option", "ICON", "wiki-icon", "lore-energy"),
+                    energyLore,
+                    String.valueOf(((EnergyNetComponent) slimefunItem).getCapacity())));
+        }
+
+        if (slimefunItem instanceof ShowInfoItem) {
+            if (((ShowInfoItem) slimefunItem).isOverride()) {
+                ItemStackUtil.setLore(infoIcon, ((ShowInfoItem) slimefunItem).getInfos());
+            } else {
+                ItemStackUtil.addLoresToLast(infoIcon, ((ShowInfoItem) slimefunItem).getInfos());
+            }
+        }
+
+        return infoIcon;
     }
 }
