6e174c18d32636138ccac1cd6cadfd433744f138
ljc0424 <1979328077@qq.com>
2023-07-03 10:47:12 +0800

    feat: new exception ItemNeedExtraArgException and ParseErrorException

---
+++ src/main/java/io/taraxacum/finaltech/core/exception/ParseErrorException.java
@@ -0,0 +1,8 @@
+package io.taraxacum.finaltech.core.exception;
+
+public class ParseErrorException extends Exception {
+
+    public ParseErrorException(String message) {
+        super(message);
+    }
+}
