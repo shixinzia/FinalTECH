96f0280c63dc799cd253618ddc5f5dca7d0f6c19
ljc0424 <1979328077@qq.com>
2023-07-03 10:12:02 +0800

    feat: new method to get recipe and recipe type of ConfigUtil

--- src/main/java/io/taraxacum/finaltech/util/ConfigUtil.java
+++ src/main/java/io/taraxacum/finaltech/util/ConfigUtil.java
@@ -2,17 +2,28 @@ package io.taraxacum.finaltech.util;
 
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.taraxacum.common.util.JavaUtil;
+import io.taraxacum.common.util.ReflectionUtil;
 import io.taraxacum.finaltech.FinalTech;
-import io.taraxacum.libs.plugin.dto.LanguageManager;
+import io.taraxacum.finaltech.core.dto.ComplexOptional;
+import io.taraxacum.finaltech.core.exception.ParseErrorException;
 import io.taraxacum.finaltech.core.group.MainItemGroup;
 import io.taraxacum.finaltech.core.group.SubFlexItemGroup;
+import io.taraxacum.finaltech.core.interfaces.ExtraParameterItem;
+import io.taraxacum.finaltech.setup.FinalTechRecipeTypes;
+import io.taraxacum.finaltech.setup.FinalTechRecipes;
+import io.taraxacum.libs.plugin.dto.ConfigFileManager;
+import io.taraxacum.libs.plugin.dto.LanguageManager;
+import io.taraxacum.libs.plugin.util.ItemStackUtil;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author Final_ROOT
@@ -65,7 +76,7 @@ public class ConfigUtil {
             material = material == null ? defaultMaterial : material;
         }
         String name = languageManager.containPath("categories", key, "name") ? languageManager.getString("categories", key, "name") : defaultName;
-        return new MainItemGroup(new NamespacedKey(languageManager.getPlugin(), key), new CustomItemStack(material, name), 0);
+        return new MainItemGroup(new NamespacedKey(languageManager.getPlugin(), key), ItemStackUtil.newItemStack(material, name), 0);
     }
 
     public static SubFlexItemGroup getSubFlexItemGroup(@Nonnull LanguageManager languageManager, @Nonnull String key, @Nonnull Material defaultMaterial, @Nonnull String defaultName) {
@@ -75,7 +86,7 @@ public class ConfigUtil {
             material = material == null ? defaultMaterial : material;
         }
         String name = languageManager.containPath("categories", key, "name") ? languageManager.getString("categories", key, "name") : defaultName;
-        return new SubFlexItemGroup(new NamespacedKey(languageManager.getPlugin(), key), new CustomItemStack(material, name), 0);
+        return new SubFlexItemGroup(new NamespacedKey(languageManager.getPlugin(), key), ItemStackUtil.newItemStack(material, name), FinalTech.getSlimefunGuideHistoryService());
     }
 
     @Nonnull
@@ -107,4 +118,84 @@ public class ConfigUtil {
         return FinalTech.getValueManager().getOrDefault(defaultValue, path);
     }
 
+    @Nonnull
+    public static ItemStack[] getRecipe(@Nonnull String id) {
+        try {
+            ConfigFileManager recipeManager = FinalTech.getRecipeManager();
+            if (!recipeManager.containPath(id)) {
+                String prefix = "FINALTECH_";
+                return ReflectionUtil.getStaticValue(FinalTechRecipes.class, id.substring(prefix.length()));
+            }
+
+            List<String> stringList = recipeManager.getStringList(id);
+            stringList.sort(Comparator.naturalOrder());
+
+            ItemStack[] itemStacks = new ItemStack[stringList.size()];
+            for (int i = 0; i < itemStacks.length; i++) {
+                String index = stringList.get(i);
+
+                String itemId = recipeManager.getOrDefault("error", id, index, "id");
+
+                if ("error".equals(itemId)) {
+                    FinalTech.logger().severe("Can not generate recipe for " + id);
+                    FinalTech.logger().severe("You need set a valid value for " + id + "." + index + ".id");
+                    return new ItemStack[0];
+                }
+
+                int amount = recipeManager.getOrDefault(1, id, index, "amount");
+
+                try {
+                    if (amount > 64 || amount <= 0) {
+                        throw new ParseErrorException("The amount should be in [0, 64]");
+                    }
+                } catch (Exception e) {
+                    FinalTech.logger().severe("Can not generate recipe for " + id);
+                    FinalTech.logger().severe("You need set a valid value for " + id + "." + index + ".amount");
+                    e.printStackTrace();
+                    return new ItemStack[0];
+                }
+
+                ComplexOptional<ItemStack> optionItemStack = MachineUtil.getItemStackById(itemId);
+
+                itemStacks[i] = optionItemStack.ok() ? optionItemStack.getOrByArgs(() -> recipeManager.getStringList(id, index, "extra-args").toArray(new String[0])) : null;
+            }
+
+            return itemStacks;
+        } catch (Exception e) {
+            FinalTech.logger().severe("An error occurred while generating recipe for " + id);
+            e.printStackTrace();
+            return new ItemStack[0];
+        }
+    }
+
+    @Nonnull
+    public static RecipeType getRecipeType(@Nonnull String id) {
+        try {
+            ItemStack[] itemStacks;
+            ConfigFileManager recipeManager = FinalTech.getRecipeManager();
+            if (!recipeManager.containPath(id)) {
+                String prefix = "FINALTECH_";
+                itemStacks = ReflectionUtil.getStaticValue(FinalTechRecipes.class, id.substring(prefix.length()));
+            } else {
+                List<String> stringList = recipeManager.getStringList(id);
+                stringList.sort(Comparator.naturalOrder());
+
+                itemStacks = new ItemStack[stringList.size()];
+            }
+
+            if (itemStacks == null) {
+                return RecipeType.NULL;
+            } else if (itemStacks.length <= 9) {
+                return FinalTechRecipeTypes.BEDROCK_CRAFT_TABLE;
+            } else if (itemStacks.length <= 36) {
+                return FinalTechRecipeTypes.MATRIX_CRAFTING_TABLE;
+            } else {
+                return RecipeType.NULL;
+            }
+        } catch (Exception e) {
+            FinalTech.logger().severe("An error occurred while generating recipe for " + id);
+            e.printStackTrace();
+            return RecipeType.NULL;
+        }
+    }
 }
