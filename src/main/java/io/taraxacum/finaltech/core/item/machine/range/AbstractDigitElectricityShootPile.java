3d2a97067db082a16bbdcf998d8416382302e931
ljc0424 <1979328077@qq.com>
2023-07-12 11:26:30 +0800

    feat: update ShootPile

--- src/main/java/io/taraxacum/finaltech/core/item/machine/range/AbstractDigitElectricityShootPile.java
+++ src/main/java/io/taraxacum/finaltech/core/item/machine/range/AbstractDigitElectricityShootPile.java
@@ -3,7 +3,6 @@ package io.taraxacum.finaltech.core.item.machine.range;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
-import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
@@ -14,7 +13,10 @@ import io.taraxacum.finaltech.core.interfaces.*;
 import io.taraxacum.finaltech.core.inventory.AbstractMachineInventory;
 import io.taraxacum.finaltech.core.inventory.unit.StatusL2Inventory;
 import io.taraxacum.finaltech.core.option.RouteShow;
-import io.taraxacum.finaltech.util.*;
+import io.taraxacum.finaltech.util.BlockTickerUtil;
+import io.taraxacum.finaltech.util.ConfigUtil;
+import io.taraxacum.finaltech.util.MachineUtil;
+import io.taraxacum.finaltech.util.RecipeUtil;
 import io.taraxacum.libs.plugin.dto.LocationData;
 import io.taraxacum.libs.plugin.util.ParticleUtil;
 import io.taraxacum.libs.slimefun.util.EnergyUtil;
@@ -26,7 +28,6 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nonnull;
@@ -38,12 +39,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * @author Final_ROOT
  */
-public class NormalConsumableElectricityShootPile extends AbstractRangeMachine implements RecipeItem, MenuUpdater, PointMachine, LineMachine, LocationMachine {
+public abstract class AbstractDigitElectricityShootPile extends AbstractRangeMachine implements RecipeItem, MenuUpdater, PointMachine, LineMachine, LocationMachine {
     private final int range = ConfigUtil.getOrDefaultItemSetting(16, this, "range");
     private final Set<String> notAllowedId = new HashSet<>(ConfigUtil.getItemStringList(this, "not-allowed-id"));
-    private int statusSlot;
+    protected int statusSlot;
 
-    public NormalConsumableElectricityShootPile(@Nonnull ItemGroup itemGroup, @Nonnull SlimefunItemStack item) {
+    public AbstractDigitElectricityShootPile(@Nonnull ItemGroup itemGroup, @Nonnull SlimefunItemStack item) {
         super(itemGroup, item);
     }
 
@@ -69,46 +70,38 @@ public class NormalConsumableElectricityShootPile extends AbstractRangeMachine i
 
     @Override
     protected void tick(@Nonnull Block block, @Nonnull SlimefunItem slimefunItem, @Nonnull LocationData locationData) {
+        int digit = this.getDigit(locationData);
         Inventory inventory = FinalTech.getLocationDataService().getInventory(locationData);
-        if(inventory == null) {
+        if (inventory == null) {
             return;
         }
         boolean hasViewer = !inventory.getViewers().isEmpty();
 
-        int digital;
-        ItemStack itemStack = inventory.getItem(this.getInputSlot()[0]);
-        SlimefunItem digitalSlimefunItem = SlimefunItem.getByItem(itemStack);
-        if(digitalSlimefunItem instanceof DigitalItem digitalItem) {
-            digital = digitalItem.getDigit();
-        } else {
-            digital = -1;
-        }
-
         BlockData blockData = block.getBlockData();
-        if (blockData instanceof Directional directional && digital != -1) {
+        if (blockData instanceof Directional directional && digit != -1) {
             boolean drawParticle = hasViewer || RouteShow.VALUE_TRUE.equals(RouteShow.OPTION.getOrDefaultValue(FinalTech.getLocationDataService(), locationData));
             Runnable runnable = () -> {
                 int capacitorEnergy = 0;
                 AtomicInteger transferEnergy = new AtomicInteger(0);
 
                 LocationData capacitorLocationData = FinalTech.getLocationDataService().getLocationData(block.getRelative(directional.getFacing().getOppositeFace()).getLocation());
-                if(capacitorLocationData != null
+                if (capacitorLocationData != null
                         && LocationDataUtil.getSlimefunItem(FinalTech.getLocationDataService(), capacitorLocationData) instanceof EnergyNetComponent energyNetComponent
                         && JavaUtil.matchOnce(energyNetComponent.getEnergyComponentType(), EnergyNetComponentType.CAPACITOR, EnergyNetComponentType.GENERATOR)) {
                     capacitorEnergy = Integer.parseInt(EnergyUtil.getCharge(FinalTech.getLocationDataService(), capacitorLocationData));
                 }
 
-                if(capacitorEnergy == 0) {
+                if (capacitorEnergy == 0) {
                     return;
                 }
 
                 RangeMachine.RangeFunction rangeFunction = location -> {
-                    if(!capacitorLocationData.getLocation().getChunk().isLoaded() || !location.getChunk().isLoaded()) {
+                    if (!capacitorLocationData.getLocation().getChunk().isLoaded() || !location.getChunk().isLoaded()) {
                         return -1;
                     }
 
                     int capacitorEnergyNow = Integer.parseInt(EnergyUtil.getCharge(FinalTech.getLocationDataService(), capacitorLocationData));
-                    if(capacitorEnergyNow <= 0) {
+                    if (capacitorEnergyNow <= 0) {
                         return -1;
                     }
 
@@ -118,7 +111,7 @@ public class NormalConsumableElectricityShootPile extends AbstractRangeMachine i
                             && LocationDataUtil.getSlimefunItem(FinalTech.getLocationDataService(), energyLocationData) instanceof EnergyNetComponent energyNetComponent
                             && !EnergyNetComponentType.NONE.equals(energyNetComponent.getEnergyComponentType())) {
                         int componentCapacity = energyNetComponent.getCapacity();
-                        if(componentCapacity > 0) {
+                        if (componentCapacity > 0) {
                             int componentEnergy = Integer.parseInt(EnergyUtil.getCharge(FinalTech.getLocationDataService(), energyLocationData));
                             if (componentEnergy < componentCapacity) {
                                 transferEnergy.set(Math.min(capacitorEnergyNow, componentCapacity - componentEnergy));
@@ -127,9 +120,7 @@ public class NormalConsumableElectricityShootPile extends AbstractRangeMachine i
                                 EnergyUtil.setCharge(FinalTech.getLocationDataService(), capacitorLocationData, String.valueOf(capacitorEnergyNow));
                             }
 
-                            itemStack.setAmount(itemStack.getAmount() - 1);
-
-                            if(drawParticle) {
+                            if (drawParticle) {
                                 final Location finalLocation = location.clone();
                                 JavaPlugin javaPlugin = this.getAddon().getJavaPlugin();
                                 javaPlugin.getServer().getScheduler().runTaskAsynchronously(javaPlugin, () -> ParticleUtil.drawCubeByBlock(javaPlugin, Particle.WAX_OFF, 0, finalLocation.getBlock()));
@@ -141,15 +132,15 @@ public class NormalConsumableElectricityShootPile extends AbstractRangeMachine i
                     return 0;
                 };
 
-                if(capacitorEnergy > 0) {
-                    if(digital == 0) {
+                if (capacitorEnergy > 0) {
+                    if (digit == 0) {
                         this.lineFunction(block, this.range, directional.getFacing(), rangeFunction);
                     } else {
-                        this.pointFunction(block, digital, rangeFunction);
+                        this.pointFunction(block, digit, rangeFunction);
                     }
                 }
 
-                if(hasViewer) {
+                if (hasViewer) {
                     this.updateInv(inventory, this.statusSlot, this,
                             String.valueOf(capacitorEnergy),
                             String.valueOf(transferEnergy));
@@ -157,13 +148,13 @@ public class NormalConsumableElectricityShootPile extends AbstractRangeMachine i
             };
 
             BlockTickerUtil.runTask(FinalTech.getLocationRunnableFactory(), FinalTech.isAsyncSlimefunItem(this.getId()), runnable, () -> {
-                if(digital > 0) {
+                if (digit > 0) {
                     Location[] locations = new Location[3];
                     locations[0] = block.getRelative(directional.getFacing().getOppositeFace()).getLocation();
                     locations[1] = block.getLocation();
-                    locations[2] = block.getRelative(directional.getFacing(), digital).getLocation();
+                    locations[2] = block.getRelative(directional.getFacing(), digit).getLocation();
                     return locations;
-                } else if(digital == 0) {
+                } else if (digit == 0) {
                     return this.getLocations(block.getLocation());
                 } else {
                     return new Location[0];
@@ -174,6 +165,29 @@ public class NormalConsumableElectricityShootPile extends AbstractRangeMachine i
                     "0",
                     "0");
         }
+    }
+
+    @Override
+    protected boolean isSynchronized() {
+        return false;
+    }
+
+    @Override
+    public Location[] getLocations(@Nonnull Location sourceLocation) {
+        Block block = sourceLocation.getBlock();
+        BlockData blockData = block.getBlockData();
+        if (blockData instanceof Directional directional) {
+            BlockFace blockFace = directional.getFacing();
+            Location[] locations = new Location[this.range + 2];
+            int i = 0;
+            locations[i++] = block.getRelative(blockFace.getOppositeFace()).getLocation();
+            locations[i++] = sourceLocation;
+            while (i < locations.length) {
+                locations[i++] = block.getRelative(blockFace, i - 2).getLocation();
+            }
+            return locations;
+        }
+        return new Location[0];
     }
 
     @Nonnull
@@ -188,30 +202,12 @@ public class NormalConsumableElectricityShootPile extends AbstractRangeMachine i
     }
 
     @Override
-    public Location[] getLocations(@Nonnull Location sourceLocation) {
-        Block block = sourceLocation.getBlock();
-        BlockData blockData = block.getBlockData();
-        if(blockData instanceof Directional directional) {
-            BlockFace blockFace = directional.getFacing();
-            Location[] locations = new Location[this.range + 2];
-            int i = 0;
-            locations[i++] = block.getRelative(blockFace.getOppositeFace()).getLocation();
-            locations[i++] = sourceLocation;
-            while (i < locations.length) {
-                locations[i++] = block.getRelative(blockFace, i - 2).getLocation();
-            }
-            return locations;
-        }
-        return new Location[0];
-    }
-
-    @Override
-    protected boolean isSynchronized() {
-        return false;
-    }
-
-    @Override
     public void registerDefaultRecipes() {
         RecipeUtil.registerDescriptiveRecipe(FinalTech.getLanguageManager(), this);
     }
+
+    /**
+     * @return -1 if we can do nothing
+     */
+    protected abstract int getDigit(@Nonnull LocationData locationData);
 }
