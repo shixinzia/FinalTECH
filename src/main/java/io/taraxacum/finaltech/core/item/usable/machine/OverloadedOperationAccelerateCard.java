848f29fde001fe6c5a7a7b161fa28bf39a1d0316
ljc0424 <1979328077@qq.com>
2023-07-12 10:47:55 +0800

    feat: new items AccelerateCard

---
+++ src/main/java/io/taraxacum/finaltech/core/item/usable/machine/OverloadedOperationAccelerateCard.java
@@ -0,0 +1,45 @@
+package io.taraxacum.finaltech.core.item.usable.machine;
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineProcessHolder;
+import io.github.thebusybiscuit.slimefun4.core.machines.MachineOperation;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.taraxacum.finaltech.FinalTech;
+import io.taraxacum.finaltech.core.interfaces.RecipeItem;
+import io.taraxacum.finaltech.util.RecipeUtil;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author Final_ROOT
+ */
+public class OverloadedOperationAccelerateCard extends AbstractOperationAccelerateCard implements RecipeItem {
+    private final boolean consumable = true;
+    public OverloadedOperationAccelerateCard(@Nonnull ItemGroup itemGroup, @Nonnull SlimefunItemStack item) {
+        super(itemGroup, item);
+    }
+
+    @Override
+    void addProgress(@Nonnull MachineOperation machineOperation) {
+        int progress = machineOperation.getRemainingTicks();
+        machineOperation.addProgress(Math.min(progress / 2, machineOperation.getTotalTicks() - machineOperation.getProgress()));
+    }
+
+    @Override
+    boolean consumable() {
+        return this.consumable;
+    }
+
+    @Override
+    public void registerDefaultRecipes() {
+        RecipeUtil.registerDescriptiveRecipeWithBorder(FinalTech.getLanguageManager(), this);
+
+        for (SlimefunItem slimefunItem : Slimefun.getRegistry().getEnabledSlimefunItems()) {
+            if (slimefunItem instanceof MachineProcessHolder && !this.notAllowedId.contains(slimefunItem.getId())) {
+                this.registerDescriptiveRecipe(slimefunItem.getItem());
+            }
+        }
+    }
+}
