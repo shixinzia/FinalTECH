65ecf91ac222cfd8961ed8eb16fb3f62f29d57dd
ecro.fun <ecro.fun@gmail.com>
2023-07-20 01:25:36 +0800

    feat: balance tweak for BasicLogicFactory

--- src/main/java/io/taraxacum/finaltech/core/item/machine/template/basic/BasicLogicFactory.java
+++ src/main/java/io/taraxacum/finaltech/core/item/machine/template/basic/BasicLogicFactory.java
@@ -3,6 +3,8 @@ package io.taraxacum.finaltech.core.item.machine.template.basic;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.taraxacum.finaltech.setup.FinalTechItemStacks;
+import io.taraxacum.libs.plugin.util.ItemStackUtil;
+import io.taraxacum.libs.slimefun.dto.RandomMachineRecipe;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -17,6 +19,8 @@ public class BasicLogicFactory extends AbstractBasicMachine {
 
     @Override
     public void registerDefaultRecipes() {
-        this.registerRecipe(new ItemStack[] {FinalTechItemStacks.LOGIC_FALSE, FinalTechItemStacks.LOGIC_TRUE, FinalTechItemStacks.BUG}, new ItemStack[]{FinalTechItemStacks.ENTROPY});
+        RandomMachineRecipe.RandomOutput randomOutput1 = new RandomMachineRecipe.RandomOutput(new ItemStack[]{(ItemStackUtil.cloneItem(FinalTechItemStacks.LOGIC_FALSE, 2))}, 1);
+        RandomMachineRecipe.RandomOutput randomOutput2 = new RandomMachineRecipe.RandomOutput(new ItemStack[]{(ItemStackUtil.cloneItem(FinalTechItemStacks.LOGIC_TRUE, 2))}, 1);
+        this.registerRecipe(new RandomMachineRecipe(new ItemStack[] {FinalTechItemStacks.LOGIC_FALSE, FinalTechItemStacks.LOGIC_TRUE}, new RandomMachineRecipe.RandomOutput[] {randomOutput1, randomOutput2}));
     }
 }
