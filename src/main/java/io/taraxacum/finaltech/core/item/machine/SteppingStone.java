11d68a6494cf4645c8488abb7671d5b5f49cd205
ecro.fun <ecro.fun@gmail.com>
2023-07-20 01:25:52 +0800

    feat: register recipe for SteppingStone

--- src/main/java/io/taraxacum/finaltech/core/item/machine/SteppingStone.java
+++ src/main/java/io/taraxacum/finaltech/core/item/machine/SteppingStone.java
@@ -5,8 +5,11 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
+import io.taraxacum.finaltech.FinalTech;
+import io.taraxacum.finaltech.core.interfaces.RecipeItem;
 import io.taraxacum.finaltech.core.inventory.AbstractMachineInventory;
 import io.taraxacum.finaltech.util.MachineUtil;
+import io.taraxacum.finaltech.util.RecipeUtil;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -17,7 +20,7 @@ import java.util.Collection;
 /**
  * @author Final_ROOT
  */
-public class SteppingStone extends AbstractMachine {
+public class SteppingStone extends AbstractMachine implements RecipeItem {
     public SteppingStone(@Nonnull ItemGroup itemGroup, @Nonnull SlimefunItemStack item) {
         super(itemGroup, item, RecipeType.NULL, new ItemStack[0]);
     }
@@ -49,5 +52,10 @@ public class SteppingStone extends AbstractMachine {
     @Override
     public Collection<ItemStack> getDrops() {
         return new ArrayList<>();
+    }
+
+    @Override
+    public void registerDefaultRecipes() {
+        RecipeUtil.registerDescriptiveRecipe(FinalTech.getLanguageManager(), this);
     }
 }
